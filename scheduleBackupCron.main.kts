fda24f1c12ca8c658e7223a26dc21fedb7010d0d
Cadeyn Rauscher <cadeynrauscher@gmail.com>
2026-07-24 17:18:26 -0700

    Start editing cronjob scheduler

---
+++ scheduleBackupCron.main.kts
@@ -0,0 +1,6 @@
+/*
+ * This script is meant meant to find and edit the cron job file to schedule the backup.
+ * It is used to backup the database and the files.
+ * It is used to backup the database and the files.
+ * 
+*/
